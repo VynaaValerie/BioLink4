05e96518c3782be671b09b341f24b57059df69c7
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-07-06 15:12:38 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  profilePicture: "https://telegra.ph/file/f034734bd0bc2fed63491.jpg",
+  popupPicture: "https://telegra.ph/file/f034734bd0bc2fed63491.jpg",
+  popupQuote: "But... the sensation that I've lost something lingers for a longtime after I wake up.",
+  userName: "@VynaaValerie",
+  links: [
+    { href: "https://github.com/VynaaVlerie", icon: "fab fa-github", label: "Github" },
+    { href: "https://www.linkedin.com/in/", icon: "fab fa-linkedin", label: "Linkedin" },
+    { href: "https://www.facebook.com/", icon: "fab fa-facebook", label: "Facebook" },
+    { href: "https://www.instagram.com/vynaa_valerie", icon: "fab fa-instagram", label: "Instagram" },
+    { href: "https://twitter.com/", icon: "fab fa-twitter", label: "Twitter" },
+    { href: "https://myanimelist.net/profile/", icon: "fa fa-play-circle", label: "MyAnimeList" }
+  ]
+};
